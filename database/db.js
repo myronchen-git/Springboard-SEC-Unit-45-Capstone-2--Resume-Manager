b9800c4c398859a14b43fe542c7b94c477c0f6a6
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-01-07 14:59:47 -0800

    feat: create database connection
    Created a file and class to handle the PostgreSQL database
    connections.  This uses a database connection pool to attempt following
    best practices and to allow future use of multiple clients.
    
    Having one connection and keeping it alive, as done in previous
    projects, works fine when there is a single connected client.  When
    having multiple clients or making frequent queries, a single connection
    becomes a bottleneck.
    
    Only integration or end-to-end tests would be needed, which will be
    done later when there are API endpoints.

---
+++ database/db.js
@@ -0,0 +1,59 @@
+/** Database setup for app. */
+
+'use strict';
+
+const { Pool } = require('pg');
+const { DATABASE_URI } = require('../config');
+
+// ==================================================
+
+/**
+ * The class that will handle making the database connection.  This also will
+ * act as an adapter.
+ */
+class PostgresDb {
+  static config = {
+    connectionString: DATABASE_URI,
+  };
+
+  /**
+   * Creates a new database connection pool.
+   */
+  constructor() {
+    this.pool = new Pool(PostgresDb.config);
+    this.pool.on('error', (err, client) => {
+      console.error('Unexpected error on idle client:', err);
+      process.exit(-1);
+    });
+  }
+
+  /**
+   * Shuts down PostgreSQL connection pool, so that there are no active
+   * connections.
+   */
+  async shutdown() {
+    await this.pool.end();
+  }
+
+  /**
+   * Acts as a liaison between the call to query the database and the app.
+   * This is used to add things like logging to each query.
+   *
+   * @param {Object} queryConfig - Contains parameters such as text (SQL
+   *  strings), values (arguments for parameterized queries), and name (for
+   *  creating prepared statements).  See
+   *  [https://node-postgres.com/apis/client#queryconfig].
+   * @returns The result from a database query.
+   */
+  async query(queryConfig) {
+    console.log('Executing query on database:', queryConfig.text);
+    const dbClient = await this.pool.connect();
+    const result = await dbClient.query(queryConfig);
+    dbClient.release();
+    return result;
+  }
+}
+
+// ==================================================
+
+module.exports = PostgresDb;
