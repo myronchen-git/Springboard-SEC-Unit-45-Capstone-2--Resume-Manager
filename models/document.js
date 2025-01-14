433749ce02469463adac812a713185ede1621d7e
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-01-13 17:48:08 -0800

    feat: implement Document model
    Implemented the model with add, getAll, get, and update methods.
    
    Template literals are indented towards the left, because it allows the
    String to be readable and consistent in both the code and logs.
    Otherwise, the logs will show significant indentations.
    
    Other solutions are no good.  Using backslashes (\) at the end of
    String lines do not remove the extra spacing in logs.  Using Arrays and
    joining the Strings together makes it less readable in the code and
    might require extra processing.

---
+++ models/document.js
@@ -0,0 +1,222 @@
+'use strict';
+
+const { db } = require('../app');
+const { AppServerError } = require('../errors/appErrors');
+const { NotFoundError } = require('../errors/appErrors');
+const { convertPropsForSqlUpdate } = require('../util/sqlHelpers');
+const logger = require('../util/logger');
+
+// ==================================================
+
+/**
+ * Represents a document.  This contains info about a document and not the
+ * actual content.
+ */
+class Document {
+  // To use in SQL statements to return all column data.
+  static _allDbDocumentColsAsJs = `
+    id,
+    document_name AS "documentName",
+    owner,
+    created_on AS "createdOn",
+    last_updated AS "lastUpdated",
+    is_master AS "isMaster",
+    is_template AS "isTemplate",
+    is_locked AS "isLocked"`;
+
+  constructor(
+    id,
+    documentName,
+    owner,
+    createdOn,
+    lastUpdated,
+    isMaster,
+    isTemplate,
+    isLocked
+  ) {
+    this.id = id;
+    this.documentName = documentName;
+    this.owner = owner;
+    this.createdOn = createdOn;
+    this.lastUpdated = lastUpdated;
+    this.isMaster = isMaster;
+    this.isTemplate = isTemplate;
+    this.isLocked = isLocked;
+  }
+
+  /**
+   * Creates a new document entry in the database.
+   *
+   * @param {Object} docProps - Contains data for creating a new document.
+   * @param {String} docProps.documentName - Name of the document.
+   * @param {String} docProps.owner - Username that the document belongs to.
+   * @param {Boolean} docProps.isMaster - If the document is the master
+   *  resume.
+   * @param {Boolean} docProps.isTemplate - If the document is a template.
+   * @returns {Document} A new Document instance that contains the document's
+   *  data.
+   */
+  static async add(docProps) {
+    const logPrefix = `Document.add(${JSON.stringify(docProps)})`;
+    logger.verbose(logPrefix);
+
+    const { documentName, owner, isMaster, isTemplate } = docProps;
+
+    const queryConfig = {
+      text: `
+  INSERT INTO documents (document_name, owner, is_master, is_template)
+  VALUES ($1, $2, $3, $4)
+  RETURNING ${Document._allDbDocumentColsAsJs};`,
+      values: [documentName, owner, isMaster, isTemplate],
+    };
+
+    let result;
+    try {
+      result = await db.query(queryConfig);
+    } catch (err) {
+      logger.error(`${logPrefix}: ${err}`);
+      throw err;
+    }
+
+    return new Document(...Object.values(result.rows[0]));
+  }
+
+  /**
+   * Retrieves all the documents belonging to a user.
+   *
+   * @param {String} owner - Username to get the documents for.
+   * @returns {Document[]} A list of Document instances.
+   */
+  static async getAll(owner) {
+    const logPrefix = `Document.getAll(${owner})`;
+    logger.verbose(logPrefix);
+
+    const queryConfig = {
+      text: `
+  SELECT ${Document._allDbDocumentColsAsJs}
+  FROM documents
+  WHERE owner = $1;`,
+      values: [owner],
+    };
+
+    let result;
+    try {
+      result = await db.query(queryConfig);
+    } catch (err) {
+      logger.error(`${logPrefix}: ${err}`);
+      throw err;
+    }
+
+    return result.rows.map((data) => new Document(...Object.values(data)));
+  }
+
+  /**
+   * Retrieves a specific document by ID or name, for a specified owner.
+   *
+   * @param {Object} queryParams - Contains the query parameters for finding a
+   *  specific document.
+   * @param {String} [queryParams.id] - ID of the document.
+   * @param {String} [queryParams.documentName] - Name of the document.
+   * @param {String} queryParams.owner - Username the document belongs to.
+   * @returns {Document} A new Document instance that contains the document's
+   *  data.
+   */
+  static async get(queryParams) {
+    const logPrefix = `Document.get(${JSON.stringify(queryParams)})`;
+    logger.verbose(logPrefix);
+
+    const { id, documentName, owner } = queryParams;
+
+    const queryConfig = {
+      text: `
+  SELECT ${Document._allDbDocumentColsAsJs}
+  FROM documents
+  WHERE ${id == undefined ? 'document_name' : 'id'} = $1 AND owner = $2;`,
+      values: [id == undefined ? documentName : id, owner],
+    };
+
+    let result;
+    try {
+      result = await db.query(queryConfig);
+    } catch (err) {
+      logger.error(`${logPrefix}: ${err}`);
+      throw err;
+    }
+
+    if (result.rows.length === 0) {
+      logger.error(`${logPrefix}: Document not found.`);
+      throw new NotFoundError(`Can not find document "${documentName}".`);
+    }
+
+    const data = result.rows[0];
+    return new Document(...Object.values(data));
+  }
+
+  /**
+   * Updates a document with new properties.  If no properties are passed, then
+   * the document is not updated.
+   *
+   * @param {Object} docProps - Contains the updated properties.
+   * @param {String} [docProps.documentName] - The new name of the document.
+   * @param {Boolean} [docProps.isMaster] - The new value for whether this
+   *  document is the master resume.
+   * @param {Boolean} [docProps.isTemplate] - The new value for whether this
+   *  document is a template.
+   * @param {Boolean} [docProps.isLocked] - The new value for whether this
+   *  document is locked.
+   * @returns {Document} The same Document instance that this method was called
+   *  on, but with updated property values.
+   */
+  async update(docProps) {
+    const logPrefix = `Document.update(${JSON.stringify(docProps)})`;
+    logger.verbose(logPrefix);
+
+    const allowedProps = ['documentName', 'isMaster', 'isTemplate', 'isLocked'];
+    const filteredProps = Object.fromEntries(
+      Object.entries(docProps).filter((prop) => allowedProps.includes(prop[0]))
+    );
+
+    if (!Object.keys(filteredProps).length) return this;
+
+    const [sqlSubstring, sqlValues] = convertPropsForSqlUpdate(filteredProps);
+
+    const queryConfig = {
+      text: `
+  UPDATE documents
+  SET ${sqlSubstring}
+    last_updated = (NOW() at time zone 'utc')
+  WHERE id = $${sqlValues.length + 1}
+  RETURNING ${Document._allDbDocumentColsAsJs};`,
+      values: [...sqlValues, this.id],
+    };
+
+    let result;
+    try {
+      result = await db.query(queryConfig);
+    } catch (err) {
+      logger.error(`${logPrefix}: ${err}`);
+      throw err;
+    }
+
+    if (result.rowCount === 0) {
+      logger.error(
+        `${logPrefix}: Document ID ${this.id} with ` +
+          `name "${this.documentName}" was not found.`
+      );
+      throw new AppServerError(
+        `Document ID ${this.id} with ` +
+          `name "${this.documentName}" was not found.`
+      );
+    }
+
+    Object.entries(result.rows[0]).forEach(([colName, val]) => {
+      this[colName] = val;
+    });
+
+    return this;
+  }
+}
+
+// ==================================================
+
+module.exports = Document;
---
+++ models/document.test.js
@@ -0,0 +1,283 @@
+'use strict';
+
+const Document = require('./document');
+const { db } = require('../app');
+const { AppServerError } = require('../errors/appErrors');
+
+// ==================================================
+
+// Mock to bypass executing app code, but use actual "db" instance.
+jest.mock('../app', () => ({
+  db: new (require('../database/db'))(),
+}));
+
+// ==================================================
+
+describe('Document', () => {
+  const existingUser = Object.freeze({ username: 'user1', password: '123' });
+
+  const newDocsProps = Object.freeze([
+    Object.freeze({
+      documentName: 'doc1',
+      owner: existingUser.username,
+      isMaster: true,
+      isTemplate: false,
+    }),
+    Object.freeze({
+      documentName: 'doc2',
+      owner: existingUser.username,
+      isMaster: false,
+      isTemplate: true,
+    }),
+  ]);
+
+  function getExpectedDocData(docProps) {
+    return {
+      id: expect.any(Number),
+      documentName: docProps.documentName,
+      owner: docProps.owner,
+      createdOn: expect.any(Date),
+      lastUpdated: null,
+      isMaster: docProps.isMaster,
+      isTemplate: docProps.isTemplate,
+      isLocked: false,
+    };
+  }
+
+  // To help with expects by directly getting data from the database.
+  const sqlTextGetAll = `
+  SELECT
+    id,
+    document_name AS "documentName",
+    owner,
+    created_on AS "createdOn",
+    last_updated AS "lastUpdated",
+    is_master AS "isMaster",
+    is_template AS "isTemplate",
+    is_locked AS "isLocked"
+  FROM documents`;
+
+  beforeAll(() => {
+    return db.query({
+      text: `
+    INSERT INTO users VALUES
+    ($1, $2);`,
+      values: [existingUser.username, existingUser.password],
+    });
+  });
+
+  beforeEach(() => {
+    return db.query({ text: 'TRUNCATE TABLE documents CASCADE;' });
+  });
+
+  afterAll((done) => {
+    db.query({ text: 'TRUNCATE TABLE users CASCADE;' })
+      .then(() => db.shutdown())
+      .then(() => done());
+  });
+
+  // -------------------------------------------------- add
+
+  describe('add', () => {
+    // Arrange
+    const docProps = newDocsProps[0];
+    const expectedDocData = getExpectedDocData(docProps);
+
+    test('Adds a new document.', async () => {
+      // Act
+      const document = await Document.add(docProps);
+
+      // Assert
+      expect(document).toBeInstanceOf(Document);
+      expect(document).toEqual(expectedDocData);
+
+      const databaseDocData = (
+        await db.query({
+          text: sqlTextGetAll + '\n  WHERE id = $1;',
+          values: [document.id],
+        })
+      ).rows[0];
+
+      expect(databaseDocData).toEqual(expectedDocData);
+    });
+
+    test('Throws an Error if adding a document with the same name as another.', async () => {
+      // Arrange
+      await Document.add(docProps);
+      const duplicateDocProps = { ...docProps, isMaster: false };
+
+      // Act
+      async function runFunc() {
+        await Document.add(duplicateDocProps);
+      }
+
+      // Assert
+      await expect(runFunc).rejects.toThrow();
+
+      const databaseDocsData = (
+        await db.query({
+          text: sqlTextGetAll + '\n  WHERE owner = $1;',
+          values: [existingUser.username],
+        })
+      ).rows;
+
+      // Ensure existing data has not been modified.
+      expect(databaseDocsData.length).toBe(1);
+      expect(databaseDocsData[0]).toEqual(expectedDocData);
+    });
+  });
+
+  // -------------------------------------------------- getAll
+
+  describe('getAll', () => {
+    test.each([
+      [2, newDocsProps],
+      [0, []],
+    ])('Get all of %i documents for a user.', async (amount, docsProps) => {
+      // Arrange
+      const expectedDocsData = [];
+
+      for (const props of docsProps) {
+        await Document.add(props);
+        expectedDocsData.push(getExpectedDocData(props));
+      }
+
+      // Act
+      const documents = await Document.getAll(existingUser.username);
+
+      // Assert
+      expect(documents.length).toBe(docsProps.length);
+
+      documents.forEach((document, i) => {
+        expect(document).toBeInstanceOf(Document);
+        expect(document).toEqual(expectedDocsData[i]);
+      });
+    });
+  });
+
+  // -------------------------------------------------- get
+
+  describe('get', () => {
+    const docProps = newDocsProps[0];
+    const expectedDocData = getExpectedDocData(docProps);
+
+    test.each([
+      ['ID', { owner: existingUser.username }],
+      [
+        'ID',
+        { documentName: docProps.documentName, owner: existingUser.username },
+      ],
+      [
+        'name',
+        { documentName: docProps.documentName, owner: existingUser.username },
+      ],
+    ])(
+      'Gets a specified document by %s from a user.',
+      async (type, queryParams) => {
+        // Arrange
+        const existingDocument = await Document.add(docProps);
+
+        if (type === 'ID') queryParams.id = existingDocument.id;
+
+        // Act
+        const document = await Document.get(queryParams);
+
+        // Assert
+        expect(document).toBeInstanceOf(Document);
+        expect(document).toEqual(expectedDocData);
+      }
+    );
+  });
+
+  // -------------------------------------------------- update
+
+  describe('update', () => {
+    // Arrange
+    const docProps = newDocsProps[0];
+    let existingDocument = null;
+
+    const updateProps = Object.freeze({
+      documentName: 'New Name',
+      isMaster: !docProps.isMaster,
+      isTemplate: !docProps.isTemplate,
+      isLocked: !docProps.isLocked,
+    });
+
+    beforeEach((done) => {
+      Document.add(docProps).then((document) => {
+        existingDocument = document;
+        done();
+      });
+    });
+
+    afterEach(() => {
+      existingDocument = null;
+    });
+
+    test.each([
+      [0, Object.freeze({})], // empty
+      [1, Object.freeze({ documentName: updateProps.documentName })], // one
+      [Object.keys(updateProps).length, updateProps], // all
+      [
+        Object.keys(updateProps).length + 1,
+        Object.freeze({ ...updateProps, isValidColumn: false }),
+      ], // extra
+    ])('Updates a document with %s properties.', async (amount, props) => {
+      // Arrange
+      const expectedUpdatedDocument = {
+        id: existingDocument.id,
+        documentName: props.documentName || existingDocument.documentName,
+        owner: existingDocument.owner,
+        createdOn: existingDocument.createdOn,
+        lastUpdated: Object.keys(props).length ? expect.any(Date) : null,
+        isMaster:
+          props.isMaster == undefined
+            ? existingDocument.isMaster
+            : props.isMaster,
+        isTemplate:
+          props.isTemplate == undefined
+            ? existingDocument.isTemplate
+            : props.isTemplate,
+        isLocked:
+          props.isLocked == undefined
+            ? existingDocument.isLocked
+            : props.isLocked,
+      };
+
+      // Act
+      const updatedDocument = await existingDocument.update(props);
+
+      // Assert
+      expect(updatedDocument).toEqual(expectedUpdatedDocument);
+
+      const databaseDocData = (
+        await db.query({
+          text: sqlTextGetAll + '\n  WHERE id = $1;',
+          values: [existingDocument.id],
+        })
+      ).rows[0];
+
+      expect(databaseDocData).toEqual(expectedUpdatedDocument);
+    });
+
+    test('Throws an Error if document is not found.', async () => {
+      // Arrange
+      const nonexistentDocument = new Document(999);
+
+      // Act
+      async function runFunc() {
+        await nonexistentDocument.update(updateProps);
+      }
+
+      // Assert
+      await expect(runFunc).rejects.toThrow(AppServerError);
+    });
+  });
+
+  // -------------------------------------------------- delete
+
+  describe('delete', () => {
+    test.todo('Deletes a document.');
+    test.todo('Does not throw an Error if document is not found.');
+  });
+});
