cc9bfdaff8187771db69c19d0c4433020d8fa9eb
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-01-19 22:22:05 -0800

    test: refactor & add database checks for User
    Used user data from common test data file instead of creating new ones.
    
    Renamed variables newUser and existingUser to testUser, to consolidate
    the needed test user data to one.
    
    Added missing database checks for the tests for register.  Checking the
    database is needed to ensure that the database also has the correct
    data.
    
    Refactored SQL statements for existing database checks to use
    variables, to reduce making changes in multiple locations.
    
    The existing database checks also now use values from the returned User
    instance from registering, because in the future, the stored database
    values might be different than what was initially given when
    registering.

--- models/user.test.js
+++ models/user.test.js
@@ -5,6 +5,8 @@ const { db } = require('../app');
 const { AppServerError } = require('../errors/appErrors');
 const { RegistrationError, SigninError } = require('../errors/userErrors');
 
+const { users } = require('./_testData');
+
 // ==================================================
 
 // Mock to bypass executing app code, but use actual "db" instance.
@@ -15,10 +17,28 @@ jest.mock('../app', () => ({
 // ==================================================
 
 describe('User', () => {
-  const existingUser = Object.freeze({ username: 'user1', password: '123' });
+  const testUser = users[0];
+
+  const expectedUserData = Object.freeze({
+    username: users[0].username,
+    password: expect.any(String),
+  });
+
+  // To help with expects by directly getting data from the database.
+  const sqlTextSelectAll = `
+  SELECT
+    username,
+    password
+  FROM users`;
+
+  // To help with expects by filtering data retrieved from the database.
+  const whereClauseToGetOne = 'WHERE username = $1';
 
   beforeEach(() => {
-    return db.query({ text: 'TRUNCATE TABLE users CASCADE;' });
+    return db.query({
+      text: `
+  TRUNCATE TABLE users CASCADE;`,
+    });
   });
 
   afterAll(() => {
@@ -28,28 +48,43 @@ describe('User', () => {
   // -------------------------------------------------- register
 
   describe('register', () => {
-    const newUser = Object.freeze({ username: 'new-user', password: '123' });
-
     test('Registers a new user.', async () => {
       // Act
-      const user = await User.register(newUser);
+      const user = await User.register(testUser);
 
       // Assert
       expect(user).toBeInstanceOf(User);
-      expect(user.username).toBe(newUser.username);
+      expect(user.username).toBe(testUser.username);
+
+      const databaseEntry = (
+        await db.query({
+          text: sqlTextSelectAll + `\n  ${whereClauseToGetOne};`,
+          values: [user.username],
+        })
+      ).rows[0];
+
+      expect(databaseEntry).toEqual(expectedUserData);
     });
 
     test('Throws an Error if username is not available.', async () => {
       // Arrange
-      await User.register(newUser);
+      await User.register(testUser);
 
       // Act
       async function runFunc() {
-        await User.register(newUser);
+        await User.register(testUser);
       }
 
       // Assert
       await expect(runFunc).rejects.toThrow(RegistrationError);
+
+      const databaseEntries = (
+        await db.query({
+          text: sqlTextSelectAll + ';',
+        })
+      ).rows;
+
+      expect(databaseEntries.length).toBe(1);
     });
   });
 
@@ -58,20 +93,20 @@ describe('User', () => {
   describe('signin', () => {
     test('Signs in a user.', async () => {
       // Arrange
-      await User.register(existingUser);
+      await User.register(testUser);
 
       // Act
-      const user = await User.signin(existingUser);
+      const user = await User.signin(testUser);
 
       // Assert
       expect(user).toBeInstanceOf(User);
-      expect(user.username).toBe(existingUser.username);
+      expect(user.username).toBe(testUser.username);
     });
 
     test('Throws an Error if username is not found.', async () => {
       // Act
       async function runFunc() {
-        await User.signin(existingUser);
+        await User.signin(testUser);
       }
 
       // Assert
@@ -80,10 +115,10 @@ describe('User', () => {
 
     test('Throws an Error if password is invalid.', async () => {
       // Arrange
-      await User.register(existingUser);
+      await User.register(testUser);
 
       const wrongCredentials = Object.freeze({
-        username: existingUser.username,
+        username: testUser.username,
         password: 'wrong password',
       });
 
@@ -102,21 +137,21 @@ describe('User', () => {
   describe('update', () => {
     test("Updates a user's info.", async () => {
       // Arrange
-      const user = await User.register(existingUser);
+      const user = await User.register(testUser);
       const updatedInfo = Object.freeze({ password: 'updated' });
 
       // Act
       await user.update(updatedInfo);
 
       // Assert
-      const userData = (
+      const databaseEntry = (
         await db.query({
-          text: 'SELECT * FROM users WHERE username = $1;',
-          values: [existingUser.username],
+          text: sqlTextSelectAll + `\n  ${whereClauseToGetOne};`,
+          values: [user.username],
         })
       ).rows[0];
 
-      expect(userData.password).toBe(updatedInfo.password);
+      expect(databaseEntry.password).toBe(updatedInfo.password);
     });
 
     test('Throws an Error if username is not found.', async () => {
@@ -139,18 +174,20 @@ describe('User', () => {
   describe('delete', () => {
     test('Deletes a user.', async () => {
       // Arrange
-      const user = await User.register(existingUser);
+      const user = await User.register(testUser);
 
       // Act
       await user.delete();
 
       // Assert
-      const data = await db.query({
-        text: 'SELECT * FROM users WHERE username = $1;',
-        values: [existingUser.username],
-      });
+      const databaseEntries = (
+        await db.query({
+          text: sqlTextSelectAll + `\n  ${whereClauseToGetOne};`,
+          values: [user.username],
+        })
+      ).rows;
 
-      expect(data.rows.length).toBe(0);
+      expect(databaseEntries.length).toBe(0);
     });
 
     test('Does not throw an Error if username is not found.', async () => {
