26e8081471b7766fbb9592d68f5992012e7dec48
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-01-19 21:42:08 -0800

    test: organize tests for User
    In user.test.js, the bulk of the code is placed in a describe block to
    keep it all together and better organized.
    
    This commit only has that change, so the white space changes do not
    flood the file's next commit.

--- models/user.test.js
+++ models/user.test.js
@@ -14,149 +14,151 @@ jest.mock('../app', () => ({
 
 // ==================================================
 
-const existingUser = Object.freeze({ username: 'user1', password: '123' });
+describe('User', () => {
+  const existingUser = Object.freeze({ username: 'user1', password: '123' });
 
-beforeEach(() => {
-  return db.query({ text: 'TRUNCATE TABLE users CASCADE;' });
-});
-
-afterAll(() => {
-  return db.shutdown();
-});
-
-// -------------------------------------------------- register
-
-describe('register', () => {
-  const newUser = Object.freeze({ username: 'new-user', password: '123' });
-
-  test('Registers a new user.', async () => {
-    // Act
-    const user = await User.register(newUser);
-
-    // Assert
-    expect(user).toBeInstanceOf(User);
-    expect(user.username).toBe(newUser.username);
+  beforeEach(() => {
+    return db.query({ text: 'TRUNCATE TABLE users CASCADE;' });
   });
 
-  test('Throws an Error if username is not available.', async () => {
-    // Arrange
-    await User.register(newUser);
-
-    // Act
-    async function runFunc() {
-      await User.register(newUser);
-    }
-
-    // Assert
-    await expect(runFunc).rejects.toThrow(RegistrationError);
-  });
-});
-
-// -------------------------------------------------- signin
-
-describe('signin', () => {
-  test('Signs in a user.', async () => {
-    // Arrange
-    await User.register(existingUser);
-
-    // Act
-    const user = await User.signin(existingUser);
-
-    // Assert
-    expect(user).toBeInstanceOf(User);
-    expect(user.username).toBe(existingUser.username);
+  afterAll(() => {
+    return db.shutdown();
   });
 
-  test('Throws an Error if username is not found.', async () => {
-    // Act
-    async function runFunc() {
-      await User.signin(existingUser);
-    }
+  // -------------------------------------------------- register
 
-    // Assert
-    await expect(runFunc).rejects.toThrow(SigninError);
-  });
+  describe('register', () => {
+    const newUser = Object.freeze({ username: 'new-user', password: '123' });
 
-  test('Throws an Error if password is invalid.', async () => {
-    // Arrange
-    await User.register(existingUser);
+    test('Registers a new user.', async () => {
+      // Act
+      const user = await User.register(newUser);
 
-    const wrongCredentials = Object.freeze({
-      username: existingUser.username,
-      password: 'wrong password',
+      // Assert
+      expect(user).toBeInstanceOf(User);
+      expect(user.username).toBe(newUser.username);
     });
 
-    // Act
-    async function runFunc() {
-      await User.signin(wrongCredentials);
-    }
+    test('Throws an Error if username is not available.', async () => {
+      // Arrange
+      await User.register(newUser);
 
-    // Assert
-    await expect(runFunc).rejects.toThrow(SigninError);
+      // Act
+      async function runFunc() {
+        await User.register(newUser);
+      }
+
+      // Assert
+      await expect(runFunc).rejects.toThrow(RegistrationError);
+    });
   });
-});
 
-// -------------------------------------------------- update
+  // -------------------------------------------------- signin
 
-describe('update', () => {
-  test("Updates a user's info.", async () => {
-    // Arrange
-    const user = await User.register(existingUser);
-    const updatedInfo = Object.freeze({ password: 'updated' });
+  describe('signin', () => {
+    test('Signs in a user.', async () => {
+      // Arrange
+      await User.register(existingUser);
 
-    // Act
-    await user.update(updatedInfo);
+      // Act
+      const user = await User.signin(existingUser);
 
-    // Assert
-    const userData = (
-      await db.query({
+      // Assert
+      expect(user).toBeInstanceOf(User);
+      expect(user.username).toBe(existingUser.username);
+    });
+
+    test('Throws an Error if username is not found.', async () => {
+      // Act
+      async function runFunc() {
+        await User.signin(existingUser);
+      }
+
+      // Assert
+      await expect(runFunc).rejects.toThrow(SigninError);
+    });
+
+    test('Throws an Error if password is invalid.', async () => {
+      // Arrange
+      await User.register(existingUser);
+
+      const wrongCredentials = Object.freeze({
+        username: existingUser.username,
+        password: 'wrong password',
+      });
+
+      // Act
+      async function runFunc() {
+        await User.signin(wrongCredentials);
+      }
+
+      // Assert
+      await expect(runFunc).rejects.toThrow(SigninError);
+    });
+  });
+
+  // -------------------------------------------------- update
+
+  describe('update', () => {
+    test("Updates a user's info.", async () => {
+      // Arrange
+      const user = await User.register(existingUser);
+      const updatedInfo = Object.freeze({ password: 'updated' });
+
+      // Act
+      await user.update(updatedInfo);
+
+      // Assert
+      const userData = (
+        await db.query({
+          text: 'SELECT * FROM users WHERE username = $1;',
+          values: [existingUser.username],
+        })
+      ).rows[0];
+
+      expect(userData.password).toBe(updatedInfo.password);
+    });
+
+    test('Throws an Error if username is not found.', async () => {
+      // Arrange
+      const updatedInfo = Object.freeze({ password: 'updated' });
+      const user = new User('nonexistent');
+
+      // Act
+      async function runFunc() {
+        await user.update(updatedInfo);
+      }
+
+      // Assert
+      await expect(runFunc).rejects.toThrow(AppServerError);
+    });
+  });
+
+  // -------------------------------------------------- delete
+
+  describe('delete', () => {
+    test('Deletes a user.', async () => {
+      // Arrange
+      const user = await User.register(existingUser);
+
+      // Act
+      await user.delete();
+
+      // Assert
+      const data = await db.query({
         text: 'SELECT * FROM users WHERE username = $1;',
         values: [existingUser.username],
-      })
-    ).rows[0];
+      });
 
-    expect(userData.password).toBe(updatedInfo.password);
-  });
-
-  test('Throws an Error if username is not found.', async () => {
-    // Arrange
-    const updatedInfo = Object.freeze({ password: 'updated' });
-    const user = new User('nonexistent');
-
-    // Act
-    async function runFunc() {
-      await user.update(updatedInfo);
-    }
-
-    // Assert
-    await expect(runFunc).rejects.toThrow(AppServerError);
-  });
-});
-
-// -------------------------------------------------- delete
-
-describe('delete', () => {
-  test('Deletes a user.', async () => {
-    // Arrange
-    const user = await User.register(existingUser);
-
-    // Act
-    await user.delete();
-
-    // Assert
-    const data = await db.query({
-      text: 'SELECT * FROM users WHERE username = $1;',
-      values: [existingUser.username],
+      expect(data.rows.length).toBe(0);
     });
 
-    expect(data.rows.length).toBe(0);
-  });
+    test('Does not throw an Error if username is not found.', async () => {
+      // Arrange
+      const user = new User('nonexistent');
 
-  test('Does not throw an Error if username is not found.', async () => {
-    // Arrange
-    const user = new User('nonexistent');
-
-    // Act
-    await user.delete();
+      // Act
+      await user.delete();
+    });
   });
 });
