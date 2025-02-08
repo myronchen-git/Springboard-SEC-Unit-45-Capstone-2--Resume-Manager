cdb6104667ce05e606f512da8f2ca1d8f03208cb
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-02-07 16:15:53 -0800

    feat: implement user sign-in route
    Added a JSON schema for input data given during user sign-in.  This
    restricts characters to a length range to be consistent with the
    registration schema.
    
    Implemented the "/api/v1/auth/signin" route, which handles user
    sign-in.  At this moment, a service layer is not necessary, but this
    needs to be reviewed again later.
    
    In auth.test.js, the outside beforeEach code for truncating the users
    table is moved into the describe block for register, because only the
    register route needs to have a cleared table before each test.  The
    sign-in route reuses the singly created user.

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const express = require('express');
 const jsonschema = require('jsonschema');
 
 const userRegisterSchema = require('../schemas/userRegister.json');
+const userSigninSchema = require('../schemas/userSignin.json');
 
 const User = require('../models/user');
 const { createJWT } = require('../util/tokens');
@@ -61,6 +62,44 @@ router.post('/register', async (req, res, next) => {
         'number, uppercase letter, lowercase letter, and symbol.';
     }
 
+    return next(err);
+  }
+});
+
+/**
+ * POST /auth/signin
+ * { username, password } => { authToken }
+ *
+ * Authorization required: none
+ *
+ * @param {String} username - Username of the existing user.
+ * @param {String} password - Password of the existing user.
+ * @returns {String} authToken - A JWT that can be used to authenticate further
+ *  requests.
+ */
+router.post('/signin', async (req, res, next) => {
+  const logPrefix = `POST /auth/signin (request body: ${JSON.stringify({
+    ...req.body,
+    password: '(password)',
+  })})`;
+  logger.verbose(logPrefix + ': BEGIN');
+
+  try {
+    const validator = jsonschema.validate(req.body, userSigninSchema);
+
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      logger.error(
+        `${logPrefix}: JSON schema validation failed.  ${errs.join(', ')}`
+      );
+      throw new BadRequestError(errs);
+    }
+
+    const user = await User.signin(req.body);
+    const authToken = createJWT(user);
+
+    return res.json({ authToken });
+  } catch (err) {
     return next(err);
   }
 });
--- routes/auth.test.js
+++ routes/auth.test.js
@@ -10,18 +10,12 @@ const { users } = require('../models/_testData');
 // ==================================================
 
 const urlPrefix = '/api/v1';
-
-beforeEach(() => {
-  return db.query({
-    text: `
-TRUNCATE TABLE users RESTART IDENTITY CASCADE;`,
-  });
-});
+const urlRegisterUser = `${urlPrefix}/auth/register`;
 
 afterAll((done) => {
   db.query({
     text: `
-TRUNCATE TABLE users RESTART IDENTITY CASCADE;`,
+  TRUNCATE TABLE users RESTART IDENTITY CASCADE;`,
   })
     .then(() => db.shutdown())
     .then(() => done());
@@ -31,8 +25,15 @@ TRUNCATE TABLE users RESTART IDENTITY CASCADE;`,
 // POST /auth/register
 
 describe('POST /auth/register', () => {
-  const url = `${urlPrefix}/auth/register`;
+  const url = urlRegisterUser;
   const user = users[0];
+
+  beforeEach(() => {
+    return db.query({
+      text: `
+  TRUNCATE TABLE users RESTART IDENTITY CASCADE;`,
+    });
+  });
 
   test('Registers a user.', async () => {
     // Act
@@ -93,6 +94,105 @@ describe('POST /auth/register', () => {
     [{ username: user.username, password: '1234567890Ab' }],
   ])(
     'Registering with invalid data returns 400 status.  Fields: %o.',
+    async (reqBody) => {
+      // Act
+      const resp = await request(app).post(url).send(reqBody);
+
+      // Assert
+      expect(resp.statusCode).toEqual(400);
+      expect(resp.body).not.toHaveProperty('authToken');
+    }
+  );
+});
+
+// --------------------------------------------------
+// POST /auth/signin
+
+describe('POST /auth/signin', () => {
+  const url = `${urlPrefix}/auth/signin`;
+  const user = users[0];
+
+  beforeAll((done) => {
+    db.query({
+      text: `
+  TRUNCATE TABLE users RESTART IDENTITY CASCADE;`,
+    })
+      .then(() =>
+        request(app).post(urlRegisterUser).send({
+          username: user.username,
+          password: user.password,
+        })
+      )
+      .then(() => done());
+  });
+
+  test('Signs in a user.', async () => {
+    // Act
+    const resp = await request(app).post(url).send({
+      username: user.username,
+      password: user.password,
+    });
+
+    // Assert
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body).toEqual({ authToken: expect.any(String) });
+  });
+
+  test.each([
+    [
+      {
+        username: user.username,
+        password: 'wrong password',
+      },
+    ],
+    [
+      {
+        username: 'nonexistent',
+        password: user.password,
+      },
+    ],
+  ])(
+    'Signing in with incorrect fields returns 401 status.  Fields: %o.',
+    async (reqBody) => {
+      // Act
+      const resp = await request(app).post(url).send(reqBody);
+
+      // Assert
+      expect(resp.statusCode).toEqual(401);
+      expect(resp.body).not.toHaveProperty('authToken');
+    }
+  );
+
+  test.each([[{ username: user.username }], [{ password: user.password }]])(
+    'Signing in a user without required fields returns 400 status.  Fields: %o.',
+    async (reqBody) => {
+      // Act
+      const resp = await request(app).post(url).send(reqBody);
+
+      // Assert
+      expect(resp.statusCode).toEqual(400);
+      expect(resp.body).not.toHaveProperty('authToken');
+    }
+  );
+
+  // Tests the JSON schema.
+  test.each([
+    [{ username: 'us', password: user.password }],
+    [
+      {
+        username: 'user123456789012345678901234567890',
+        password: user.password,
+      },
+    ],
+    [{ username: user.username, password: '1' }],
+    [
+      {
+        username: user.username,
+        password: '12345678901234567890Ab!',
+      },
+    ],
+  ])(
+    'Signing in with invalid data returns 400 status.  Fields: %o.',
     async (reqBody) => {
       // Act
       const resp = await request(app).post(url).send(reqBody);
