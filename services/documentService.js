06c01b2d46955bbaa54555b0366e08cd454a4893
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-02-18 15:51:13 -0800

    feat: implement document update service
    Since there was some complex logic for the document update route, it
    was abstracted/moved to a service-level file.
    
    In updateDocument, the document to update is retrieved first and
    verified if it belongs to the user that wants to update it.  This is
    done to prevent users from deleting anyone's documents.

--- services/documentService.js
+++ services/documentService.js
@@ -12,6 +12,40 @@ const logger = require('../util/logger');
 // ==================================================
 
 /**
+ * Updates a document by first verifying that it belongs to the specified user.
+ *
+ * @param {String} username - Name of user that wants to update the document.
+ * @param {Number} documentId - ID of the document to be updated.
+ * @param {Object} props - Properties of the document to be updated.
+ * @returns {Document} An Document instance containing the updated info.
+ * @throws {ForbiddenError} If the document does not belong to the specified
+ *  user.
+ */
+async function updateDocument(username, documentId, props) {
+  const logPrefix =
+    `${fileName}.updateDocument(` +
+    `username = "${username}", ` +
+    `documentId = ${documentId}, ` +
+    `props = ${JSON.stringify(props)})`;
+  logger.verbose(logPrefix);
+
+  const document = await Document.get({ id: documentId });
+
+  if (document.owner !== username) {
+    logger.error(
+      `${logPrefix}: User "${username}" attempted to update document ` +
+        `with ID ${documentId}, which belongs to "${document.owner}".`
+    );
+    throw new ForbiddenError(
+      `Can not update document with ID ${documentId}, ` +
+        'as it belongs to another user.'
+    );
+  }
+
+  return await document.update(props);
+}
+
+/**
  * Deletes a document by first verifying that it belongs to the specified user.
  *
  * @param {String} username - Name of user that wants to delete the document.
@@ -53,4 +87,4 @@ async function deleteDocument(username, documentId) {
 
 // ==================================================
 
-module.exports = { deleteDocument };
+module.exports = { updateDocument, deleteDocument };
--- services/documentService.test.js
+++ services/documentService.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Document = require('../models/document');
-const { deleteDocument } = require('./documentService');
+const { updateDocument, deleteDocument } = require('./documentService');
 
 const { ForbiddenError, NotFoundError } = require('../errors/appErrors');
 
@@ -10,6 +10,96 @@ const { ForbiddenError, NotFoundError } = require('../errors/appErrors');
 jest.mock('../models/document');
 
 // ==================================================
+
+describe('updateDocument', () => {
+  const username = 'user1';
+  const documentId = '1';
+  const props = Object.freeze({
+    documentName: 'New name',
+    isTemplate: false,
+    isLocked: true,
+  });
+
+  const mockGet = jest.fn();
+  const mockUpdate = jest.fn();
+
+  beforeEach(() => {
+    mockGet.mockReset();
+    mockUpdate.mockReset();
+  });
+
+  test('Calls document.update if document is found and belongs to user.', async () => {
+    // Arrange
+    const document = { owner: username };
+
+    Document.get = mockGet.mockResolvedValue(document);
+    document.update = mockUpdate.mockResolvedValue(document);
+
+    Object.freeze(document);
+
+    // Act
+    const updatedDocument = await updateDocument(username, documentId, props);
+
+    // Assert
+    expect(updatedDocument).toBe(document);
+    expect(Document.get).toHaveBeenCalledWith({ id: documentId });
+    expect(document.update).toHaveBeenCalledWith(props);
+  });
+
+  test('Throws an Error if document does not belong to user.', async () => {
+    // Arrange
+    const document = { owner: 'otherUser' };
+
+    Document.get = mockGet.mockResolvedValue(document);
+    document.update = mockUpdate.mockResolvedValue(document);
+
+    // Act
+    async function runFunc() {
+      await updateDocument(username, documentId, props);
+    }
+
+    // Assert
+    await expect(runFunc).rejects.toThrow(ForbiddenError);
+    expect(Document.get).toHaveBeenCalledWith({ id: documentId });
+    expect(document.update).not.toHaveBeenCalled();
+  });
+
+  test('Passes along other Errors from Document.get.', async () => {
+    // Arrange
+    const document = { owner: username };
+
+    Document.get = mockGet.mockRejectedValue(new Error());
+    document.update = mockUpdate.mockResolvedValue(document);
+
+    // Act
+    async function runFunc() {
+      await updateDocument(username, documentId, props);
+    }
+
+    // Assert
+    await expect(runFunc).rejects.toThrow();
+    expect(Document.get).toHaveBeenCalledWith({ id: documentId });
+    expect(document.update).not.toHaveBeenCalled();
+  });
+
+  test('Passes along other Errors from Document.update.', async () => {
+    // Arrange
+    const document = { owner: username };
+
+    Document.get = mockGet.mockResolvedValue(document);
+    document.update = mockUpdate.mockRejectedValue(new Error());
+
+    // Act
+    async function runFunc() {
+      await updateDocument(username, documentId, props);
+    }
+
+    // Assert
+    await expect(runFunc).rejects.toThrow();
+    expect(Document.get).toHaveBeenCalledWith({ id: documentId });
+    expect(document.update).toHaveBeenCalledWith(props);
+  });
+});
 
 describe('deleteDocument', () => {
   const username = 'user1';
