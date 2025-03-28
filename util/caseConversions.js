7a00bb5f4668238ae005772a6561ef972168131d
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-03-27 17:50:22 -0700

    feat: create utility to convert pascal strings
    Created a utility function to convert pascal Strings into a
    space-separated String.  This is originally created to help convert
    Class names to something more readable in logs.
    
    Tests are skipped for now due to time.

--- util/caseConversions.js
+++ util/caseConversions.js
@@ -13,6 +13,19 @@ function camelToSnakeCase(str) {
   return str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
 }
 
+/**
+ * Converts a String from pascal case to normal space-separated words.  Note
+ * that each upper case letter should represent a beginning of a new word.
+ *
+ * @param {String} str - Pascal case String to convert from.
+ * @returns {String} A String with separated words.
+ */
+function pascalToSpaceSeparated(str) {
+  return str
+    .replace(/[A-Z]/g, (letter) => ` ${letter.toLowerCase()}`)
+    .trimStart();
+}
+
 // ==================================================
 
-module.exports = { camelToSnakeCase };
+module.exports = { camelToSnakeCase, pascalToSpaceSeparated };
